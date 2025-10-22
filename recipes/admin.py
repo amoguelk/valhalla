4715c73094e666d4ded368ae7fc3ed85dcf1f295
Andrea Moguel Krause <31602026+amoguelk@users.noreply.github.com>
2025-10-21 18:31:25 -0600

    Modified admin page to show ingredient relations

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
 from . import models
 
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = models.RecipeIngredient
+    extra = 0
+    show_change_link = True
+
+
 class CategoryAdmin(admin.ModelAdmin):
-    readonly_fields=["created_at", "updated_at"]
+    readonly_fields = ["created_at", "updated_at"]
+
 
 class RecipeAdmin(admin.ModelAdmin):
-    readonly_fields=["created_at", "updated_at"]
-    list_filter=["category"]
+    readonly_fields = ["created_at", "updated_at"]
+    list_filter = ["category"]
+    inlines = [RecipeIngredientInline]
+
 
 admin.site.register(models.Category, CategoryAdmin)
 admin.site.register(models.Recipe, RecipeAdmin)
